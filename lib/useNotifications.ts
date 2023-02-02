12ecde88eab2e8f5997586ef45fad93402b8f8d6
gowrisn kotari <gowrish@eflairwebtech.com>
2023-02-02 10:32:43 +0530

    added error handling to eachnotificatoin updation

--- lib/useNotifications.ts
+++ lib/useNotifications.ts
@@ -13,12 +13,21 @@ export default function useNotifications({
     data: notifications,
     mutate: mutateNotifications,
     isLoading: notificationsIsLoading,
-  } = useSWR<INotification[]>(`/api/notifications?user=${userId}`, {
-    // refreshInterval: 1000,
-    // revalidateIfStale: true,
-    // revalidateOnFocus: true,
-    // revalidateOnReconnect: true,
-  });
+  } = useSWR<INotification[]>(`/api/notifications?user=${userId}`,{
+    onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
+        // Never retry on 404.
+        // if (error.status === 404) return;
+
+        // Never retry for a specific key.
+        // if (key === "/api/user") return;
+
+        // Only retry up to 10 times.
+        if (retryCount >= 3 ) return;
+
+        // Retry after 5 seconds.
+        setTimeout(() => revalidate({ retryCount }), 5000);
+    },
+});
 
   return { notifications, mutateNotifications, notificationsIsLoading };
 }
