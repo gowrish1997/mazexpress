cbc3710c10f7b7bd7bbdbebe9d598345b985bb45
goldmonger <raunakhebballi@gmail.com>
2023-03-11 15:58:03 +0530

    changes in env

--- lib/session.ts
+++ lib/session.ts
@@ -2,8 +2,8 @@
 import type { IronSessionOptions } from "iron-session";
 
 export const sessionOptions: IronSessionOptions = {
-  // password: process.env.AUTH_SECRET as string,
-  password: '2gyZ3GDw3LHZQKDhPmPDL3sjREVRXPr8',
+  password: process.env.SECRET_COOKIE_PASSWORD as string,
+  // password: '2gyZ3GDw3LHZQKDhPmPDL3sjREVRXPr8',
   cookieName: "Maz app cookie",
   cookieOptions: {
     secure: process.env.NODE_ENV === "production" ? true : false,
