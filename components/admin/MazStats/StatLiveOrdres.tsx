11eba478c2bc8c2f726c8e6375acde80b5274562
goldmonger <raunakhebballi@gmail.com>
2023-03-13 14:15:49 +0530

    sessions update;

--- components/admin/MazStats/StatLiveOrdres.tsx
+++ components/admin/MazStats/StatLiveOrdres.tsx
@@ -15,7 +15,7 @@ const StatLiveOrdres = () => {
     <div className="flex-type6 w-2/3 flex-1 p-[10px] mr-[1px] border-[1px] border-[#BBC2CF] rounded-[4px] h-full">
       <div className="w-full flex-type3">
         <p className="text-[14px] text-[#18181B] font-[600] leading-[24px]">
-          Live Orders[{(orders as Order[]).length}]
+          Live Orders
         </p>
         <Link href="/admin/live-orders">
           <p className="text-[12px] text-[#3672DF] font-[500] leading-[21px] cursor-pointer">{`See All Live Orders >`}</p>
