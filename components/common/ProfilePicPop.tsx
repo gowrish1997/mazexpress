380caa617e0ab66f63cf9c4f5200166331d8d8ae
goldmonger <raunakhebballi@gmail.com>
2023-03-20 17:17:17 +0530

    added header

--- components/common/ProfilePicPop.tsx
+++ components/common/ProfilePicPop.tsx
@@ -77,6 +77,7 @@ const ProfilePicPop = (props: IProp) => {
       axios
         .post("https://mazapi.easydesk.work/api/upload-user-image", formData, {
           method: "POST",
+          headers: { "Content-Type": "multipart/form-data" },
         })
         .then((response) => {
           if (response.data.ok === true) {
