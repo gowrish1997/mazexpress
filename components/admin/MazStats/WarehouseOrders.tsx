83c205724fbf3f10e834b04474aab020642feb93
gowrisn kotari <gowrish@eflairwebtech.com>
2023-04-20 10:42:51 +0530

    minor changes

--- components/admin/MazStats/WarehouseOrders.tsx
+++ components/admin/MazStats/WarehouseOrders.tsx
@@ -3,62 +3,63 @@ import StatCard from "./StatCard";
 import MazStatsDropddown from "./MazStatsDropddown";
 import useOrders from "@/lib/hooks/useOrders";
 import useOrderCount from "@/lib/hooks/useOrderCount";
-const options = [
-  { value: "all", label: "All" },
-  { value: "BNG", label: "Benghazi" },
-  { value: "MIS", label: "Misrata" },
-  { value: "TRI", label: "Tripoli" },
-];
+// const options = [
+//     { value: "all", label: "All" },
+//     { value: "BNG", label: "Benghazi" },
+//     { value: "MIS", label: "Misrata" },
+//     { value: "TRI", label: "Tripoli" },
+// ];
+const options = [{ value: "istanbul", label: "istanbul" }];
 
 const WarehouseOrders = () => {
-  const [selectedDate, setSelectedDate] = useState("");
-  const [citySelection, setCitySelection] = useState<string[]>([
-    "TRI",
-    "MIS",
-    "BNG",
-  ]);
-  const { orderCount, mutateOrderCount, orderCountIsLoading, orderCountError } =
-    useOrderCount({
-      city: citySelection.length > 0 ? citySelection : undefined,
+    const [selectedDate, setSelectedDate] = useState("");
+    const [citySelection, setCitySelection] = useState<string[]>(["istanbul"]);
+    const {
+        orderCount,
+        mutateOrderCount,
+        orderCountIsLoading,
+        orderCountError,
+    } = useOrderCount({
+        // city: citySelection.length > 0 ? citySelection : undefined,
     });
 
-  const wareHouseChangeHanlder = (value: string | number) => {
-    if (value === "all") {
-      // set all
-      if (citySelection.length === 3) {
-        // unselect
-        setCitySelection([]);
-      } else {
-        setCitySelection(["TRI", "MIS", "BNG"]);
-      }
-    } else {
-      setCitySelection((prev) => {
-        if (prev.includes(value as string)) {
-          return prev.filter((el) => el !== value);
-        } else {
-          return [...prev, value as string];
-        }
-      });
-    }
-  };
-  return (
-    <StatCard>
-      <div className="w-full flex-type3">
-        <p className="text-[12px] text-[#8794AD] font-[600] leading-[18px] ">
-          Warehouse Orders
-        </p>
-        <MazStatsDropddown
-          options={options}
-          header="city"
-          onChange={wareHouseChangeHanlder}
-          selection={citySelection}
-        />
-      </div>
-      <p className="text-[24px] text-[#18181B] font-[700] leading-[32px] ">
-        {orderCount}
-      </p>
-    </StatCard>
-  );
+    // const wareHouseChangeHanlder = (value: string | number) => {
+    //     if (value === "all") {
+    //         // set all
+    //         if (citySelection.length === 3) {
+    //             // unselect
+    //             setCitySelection([]);
+    //         } else {
+    //             setCitySelection(["TRI", "MIS", "BNG"]);
+    //         }
+    //     } else {
+    //         setCitySelection((prev) => {
+    //             if (prev.includes(value as string)) {
+    //                 return prev.filter((el) => el !== value);
+    //             } else {
+    //                 return [...prev, value as string];
+    //             }
+    //         });
+    //     }
+    // };
+    return (
+        <StatCard>
+            <div className="w-full flex-type3">
+                <p className="text-[12px] text-[#8794AD] font-[600] leading-[18px] ">
+                    Warehouse Orders
+                </p>
+                <MazStatsDropddown
+                    options={options}
+                    header="city"
+                    // onChange={wareHouseChangeHanlder}
+                    selection={citySelection}
+                />
+            </div>
+            <p className="text-[24px] text-[#18181B] font-[700] leading-[32px] ">
+                {orderCount}
+            </p>
+        </StatCard>
+    );
 };
 
 export default WarehouseOrders;
