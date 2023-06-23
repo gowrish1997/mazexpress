5a5f4e3db34e8f8a0aad38b1f6001d2801a333b5
Senthil Kumar <kotarigowrish@gmail.com>
2023-06-23 12:51:57 +0530

    configured

--- components/admin/BlankPage.tsx
+++ components/admin/BlankPage.tsx
@@ -15,8 +15,8 @@ const BlankPage = () => {
       </div>
       <div className=" w-[375px] h-[122px] text-[21px] text-[#8794AD] font-[600] leading-[33px] mt-[20px] text-center ">
         {router.pathname.includes("enquiry-base")
-          ? "Oops, there are no orders on your list yet..."
-          : "Oops, there are no enquiry on your list yet..."}
+          ? " Oops, there are no enquiry on your list yet..."
+          : "Oops, there are no orders on your list yet..."}
 
         <br />
       </div>
