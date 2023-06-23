99c89882b3de3acfc4beef9f95074355f7a826d1
Senthil Kumar <kotarigowrish@gmail.com>
2023-06-23 13:05:16 +0530

    changed help center admin page

--- pages/admin/help-center.tsx
+++ pages/admin/help-center.tsx
@@ -9,91 +9,99 @@ import { useState } from "react";
 import AdminPageWrapper from "@/components/common/AdminPageWrapper";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../api/auth/[...nextauth]";
+import PageHeader from "@/components/common/PageHeader";
+import { useTranslation } from "next-i18next";
+import Layout from "@/components/layout";
 
 const HelpCenter = () => {
-    const {
-        helpCenters,
-        mutateHelpCenter,
-        helpCenterIsLoading,
-        helpCenterError,
-    } = useHelpCenter();
+  const { t } = useTranslation("common");
+  const {
+    helpCenters,
+    mutateHelpCenter,
+    helpCenterIsLoading,
+    helpCenterError,
+  } = useHelpCenter();
 
-    const [showAddNewHelpCenterModal, setShowAddNewAddressModal] =
-        useState(false);
-    const toggleAddNewHelpCenterModal = () => {
-        setShowAddNewAddressModal((prev) => !prev);
-    };
+  const [showAddNewHelpCenterModal, setShowAddNewAddressModal] =
+    useState(false);
+  const toggleAddNewHelpCenterModal = () => {
+    setShowAddNewAddressModal((prev) => !prev);
+  };
 
-    if (helpCenterIsLoading) {
-        return <LoadingPage />;
-    }
-    return (
-        <AdminPageWrapper>
-            {helpCenters?.data?.length > 0 && (
-                <HelpCenterView
-                    data={helpCenters?.data}
-                    mutateHelpCenter={mutateHelpCenter}
-                />
-            )}
-            <div>
-            <button
-                className="text-[#FFFFFF] text-[14px] leading-[21px] font-[500] bg-[#35C6F4] rounded-[4px] p-[10px] mt-[25px]  "
-                onClick={toggleAddNewHelpCenterModal}
-            >
-                + Add new
-            </button>
-
-            </div>
-          
-            {showAddNewHelpCenterModal && (
-                <AddNewHelpCenterModal
-                    close={toggleAddNewHelpCenterModal}
-                    mutateHelpCenter={mutateHelpCenter}
-                />
-            )}
-        </AdminPageWrapper>
-    );
+  if (helpCenterIsLoading) {
+    return <LoadingPage />;
+  }
+  return (
+    <AdminPageWrapper>
+      <PageHeader
+        content={t("helpCenterPage.pageHeader.Title")}
+        title="Our Help centers | MazExpress"
+      />
+      <Layout>
+        {helpCenters?.data?.length > 0 && (
+          <HelpCenterView
+            data={helpCenters?.data}
+            mutateHelpCenter={mutateHelpCenter}
+          />
+        )}
+        <div>
+          <button
+            className="text-[#FFFFFF] text-[14px] leading-[21px] font-[500] bg-[#35C6F4] rounded-[4px] p-[10px] mt-[25px]  "
+            onClick={toggleAddNewHelpCenterModal}
+          >
+            + Add new
+          </button>
+        </div>
+      </Layout>
+      {showAddNewHelpCenterModal && (
+        <AddNewHelpCenterModal
+          close={toggleAddNewHelpCenterModal}
+          mutateHelpCenter={mutateHelpCenter}
+        />
+      )}
+    </AdminPageWrapper>
+  );
 };
 
 export default HelpCenter;
 
 export async function getServerSideProps(ctx: GetServerSidePropsContext) {
-    if (process.env.NODE_ENV === "development") {
-        await i18n?.reloadResources();
-    }
+  if (process.env.NODE_ENV === "development") {
+    await i18n?.reloadResources();
+  }
 
-    const session = await getServerSession<any>(ctx.req, ctx.res, authOptions);
-    // const { pathname } = ctx.req.url;
-  
-    if (!session) {
-        return {
-            redirect: {
-                destination: `/auth/gate?mode=1`,
-                permanent: false,
-            },
-        };
-    }
+  const session = await getServerSession<any>(ctx.req, ctx.res, authOptions);
+  // const { pathname } = ctx.req.url;
 
-    if (ctx.locale == "ar" && ((session as any)?.user as any).is_admin) {
-        return {
-            redirect: {
-                destination: `${ctx.resolvedUrl}`,
-                permanent: false,
-            },
-        };
-    }
-    if (!((session as any)?.user as any).is_admin) {
-        return {
-            redirect: {
-                destination: `/`,
-                permanent: false,
-            },
-        };
-    }
-
+  if (!session) {
     return {
-        props: {
-            ...(await serverSideTranslations(ctx.locale, ["common"])),
-        },
+      redirect: {
+        destination: `/auth/gate?mode=1`,
+        permanent: false,
+      },
     };
+  }
+
+  if (ctx.locale == "ar" && ((session as any)?.user as any).is_admin) {
+    return {
+      redirect: {
+        destination: `${ctx.resolvedUrl}`,
+        permanent: false,
+      },
+    };
+  }
+  if (!((session as any)?.user as any).is_admin) {
+    return {
+      redirect: {
+        destination: `/`,
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      ...(await serverSideTranslations(ctx.locale, ["common"])),
+    },
+  };
 }
